90b01f32d8335f116441e356705c1d9d7b913afb
Petros Papadopoulos <petros.papadopoulos@ams-ix.net>
2015-04-17 19:38:01 +0200

    Added the default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,3 +16,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+user_account "deployer" do
+  ssh_keys "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDEIKPYl5frwAvMwVFVgZ/2WF0l7dhe3v+8FAVpOsMuJRFAGQuORPZ5ghsyaBYyRqrrKeUS71uhO70JuQQaT1QjkZ+N7BXhlO/JPuf+XZgn//P0LKAzdXm3UG2tWyJVrGPENViTtsk0nxGzVf//Zx61b3YXxf4M5B7STx92vPKRPuuL9ERigqrdm/4EBtpr+LR3zInXdzX6rQfBKwSJcy8qdFhNEIKhOJKiOfgA68REVKor18shz2vJJjMQVk/g8dO1/2ruCPagnlEMQzOFOU4jqm9iFjAtD1NMOassPvOi3DGv7VbUkuX7WdmhjDHDT8eFExncT6jtBZM5+cD6ACDb cacophoniks@gmail.com"
+end
+
+group "www-data" do
+  members ["deployer"]
+  append true
+end
